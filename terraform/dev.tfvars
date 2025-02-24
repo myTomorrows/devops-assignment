ec8625a1dcbacfb6620daa629100d63ac749f990
enriquefrade <enriquefrade@gmail.com>
2025-02-24 10:08:25 +0100

    add tfvars and remove defaults vars

---
+++ terraform/dev.tfvars
@@ -0,0 +1,8 @@
+environment         = "dev"
+desired_size        = 1
+max_size            = 3
+min_size            = 1
+vpc_cidr            = "10.0.0.0/16"
+public_subnet_cidrs = ["10.0.1.0/24", "10.0.2.0/24"]
+availability_zones  = ["eu-west-1a", "eu-west-1b"]
+app_version         = "0.1"
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,48 +1,40 @@
 variable "environment" {
-  description = "Environment name (e.g., dev, staging, prod)"
+  description = "Environment name (e.g., dev, stg, prd)"
   type        = string
-  default     = "dev"
 }
 
 variable "desired_size" {
   description = "Desired number of worker nodes"
   type        = number
-  default     = 1
 }
 
 variable "max_size" {
   description = "Maximum number of worker nodes"
   type        = number
-  default     = 3
 }
 
 variable "min_size" {
   description = "Minimum number of worker nodes"
   type        = number
-  default     = 1
 }
 
 variable "vpc_cidr" {
   description = "cidr block for vpc"
   type        = string
-  default     = "10.0.0.0/16"
 }
 
 variable "public_subnet_cidrs" {
   description = "cidrs for public subnets"
   type        = list(string)
-  default     = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
 
 variable "availability_zones" {
   description = "Availability zones"
   type        = list(string)
-  default     = ["eu-west-1a", "eu-west-1b"]
 }
 
 variable "app_version" {
   description = "App version"
   type        = string
-  default     = "0.2"
 }
